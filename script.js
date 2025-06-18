fed44c404413a69fd84abd0524c015641b361646
Abhimax39 <kumarpawan6878@gmail.com>
2025-06-18 19:21:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function toggleMenu() {
+  const nav = document.getElementById('navMenu');
+  nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+}
+
+function prevSlide() {
+  alert("Previous slide");
+}
+
+function nextSlide() {
+  alert("Next slide");
+}
